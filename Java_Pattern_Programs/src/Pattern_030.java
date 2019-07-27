e37fc38ae613b5e8bccdb3cdec9d2984a2878e25
Yan <yan.bondarenko@gmail.com>
2019-07-27 15:53:14 +0300

    Add 30th pattern

---
+++ Java_Pattern_Programs/src/Pattern_030.java
@@ -0,0 +1,22 @@
+public class Pattern_030 {
+    public static void main(String[] args) {
+        // 30
+        // 5 5 5 5 5
+        //   4 4 4 4
+        //     3 3 3
+        //       2 2
+        //         1
+        System.out.println("----------30");
+        for (int i = 5; i >= 1; i--) {
+            for (int ads = 4; ads > i - 1; ads--) {
+                System.out.print("  ");
+            }
+
+            for (int afw = i; afw >= 1; afw--) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
