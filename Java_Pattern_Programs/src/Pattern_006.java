032d4a32cbdb0dd40f5b81d0dad87d401c42f8a1
Yan <yan.bondarenko@gmail.com>
2019-10-12 14:26:04 +0300

    Add 6th pattern

--- Java_Pattern_Programs/src/Pattern_006.java
+++ Java_Pattern_Programs/src/Pattern_006.java
@@ -7,5 +7,13 @@ public class Pattern_006 {
          * 2 2 2 2 2
          * 1 1 1 1 1
          * */
+        for (int i = 0, digitToConsole = 5; i < 5; i++) {
+            for (int a = 0; a < 5; a++) {
+                System.out.print(digitToConsole + " ");
+            }
+
+            digitToConsole--;
+            System.out.println();
+        }
     }
 }
