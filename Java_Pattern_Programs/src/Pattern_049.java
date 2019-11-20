dc9d506916aca7dcd47fba71ac00a85a5b671e16
Yan <yan.bondarenko@gmail.com>
2019-11-20 21:57:39 +0200

    Starting 49th pattern

---
+++ Java_Pattern_Programs/src/Pattern_049.java
@@ -0,0 +1,12 @@
+public class Pattern_049 {
+    public static void main(String[] args) {
+        /*
+        * 49
+        * 9 9 9 9 9 9 9 9 9
+        *   7 7 7 7 7 7 7
+        *     5 5 5 5 5
+        *       3 3 3
+        *         1
+        * */
+    }
+}
