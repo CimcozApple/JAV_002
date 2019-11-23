e47d680552f185f8d606ef6c1ad598d25800998f
Yan <yan.bondarenko@gmail.com>
2019-11-23 17:30:20 +0200

    Finish 49th pattern

--- Java_Pattern_Programs/src/Pattern_049.java
+++ Java_Pattern_Programs/src/Pattern_049.java
@@ -7,6 +7,27 @@ public class Pattern_049 {
         *     5 5 5 5 5
         *       3 3 3
         *         1
+        * 'height' value can be changed from 1 up to 5
         * */
+        int height = 5;
+        int space = 0;
+        int digit = height * 2 - 1;
+        int numberOfDigits = digit;
+
+        for (int i = 0; i < height; i++) {
+            for (int a = 0; a < space; a++) {
+                System.out.print("  ");
+            }
+
+            space++;
+
+            for (int b = 0; b < numberOfDigits; b++) {
+                System.out.print(digit + " ");
+            }
+
+            digit -= 2;
+            numberOfDigits -= 2;
+            System.out.println();
+        }
     }
 }
