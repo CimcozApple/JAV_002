ef041c9709e87e0030c11fe0fbb28d40086d2c5e
Yan <yan.bondarenko@gmail.com>
2019-11-25 23:53:20 +0200

    Started 50th pattern

---
+++ Java_Pattern_Programs/src/Pattern_050.java
@@ -0,0 +1,33 @@
+public class Pattern_050 {
+    public static void main(String[] args) {
+        /*
+        * 50
+        * 1 2 3 4 5 6 7 8 9
+        *   1 2 3 4 5 6 7
+        *     1 2 3 4 5
+        *       1 2 3
+        *         1
+        * */
+        int heightOfUpsideDownPyramid = 3;
+        int space = 0;
+        for (int i = 0; i < heightOfUpsideDownPyramid; i++) {
+            for (int a = 0; a < space; a++) {
+                System.out.print("  ");
+            }
+
+            space++;
+
+
+            for (int b = 0; b < ...) {
+
+            }
+
+
+
+
+
+
+            System.out.println();
+        }
+    }
+}
