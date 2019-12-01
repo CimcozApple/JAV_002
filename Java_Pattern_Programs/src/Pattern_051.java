19aa590931eaf19483d7310e714eb16deadef8a9
Yan <yan.bondarenko@gmail.com>
2019-12-01 20:42:06 +0200

    Starting 51th pattern

---
+++ Java_Pattern_Programs/src/Pattern_051.java
@@ -0,0 +1,14 @@
+public class Pattern_051 {
+    public static void main(String[] args) {
+        /*
+        * 51
+        * E E E E E E E E E
+        *   D D D D D D D
+        *     C C C C C
+        *       B B B
+        *         A
+        * */
+
+
+    }
+}
