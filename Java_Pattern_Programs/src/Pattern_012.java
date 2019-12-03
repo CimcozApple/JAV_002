7d2772241dcc305aadac848b8758df451868f6e8
Yan <yan.bondarenko@gmail.com>
2019-12-03 21:02:17 +0200

    Refactor 12th pattern

--- Java_Pattern_Programs/src/Pattern_012.java
+++ Java_Pattern_Programs/src/Pattern_012.java
@@ -8,12 +8,11 @@ public class Pattern_012 {
          * 1 2 3 4
          * 1 2 3 4 5
          * */
-        int foo3 = 1;
-        int fooE3 = 5;
-        for (int i = foo3; i <= fooE3; i++) {
+        for (int i = 1; i <= 5; i++) {
             for (int a = 1; a <= i; a++) {
                 System.out.print(a + " ");
             }
+
             System.out.println();
         }
     }
