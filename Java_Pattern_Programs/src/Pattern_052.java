550e10494e30871f5716f6b6c9178dc462721d26
Yan <yan.bondarenko@gmail.com>
2019-12-07 18:07:43 +0200

    Starting 52nd pattern

---
+++ Java_Pattern_Programs/src/Pattern_052.java
@@ -0,0 +1,12 @@
+public class Pattern_052 {
+    public static void main(String[] args) {
+        /*
+        * 52
+        * I I I I I I I I I
+        *   G G G G G G G
+        *     E E E E E
+        *       C C C
+        *         A
+        * */
+    }
+}
