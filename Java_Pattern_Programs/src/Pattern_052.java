3aaa130a98ceb254d6e83ef5a1efff2eea57b86c
Yan <yan.bondarenko@gmail.com>
2019-12-12 23:30:04 +0200

    Finish 52nd pattern

--- Java_Pattern_Programs/src/Pattern_052.java
+++ Java_Pattern_Programs/src/Pattern_052.java
@@ -7,6 +7,27 @@ public class Pattern_052 {
         *     E E E E E
         *       C C C
         *         A
+        *
         * */
+        int heightOfPyramid = 5;
+        int space = 0;
+        int letter = (65 + (heightOfPyramid * 2)) - 2;
+        int count = heightOfPyramid * 2 - 1;
+
+        for (int i = 0; i < heightOfPyramid; i++) {
+            for (int a = 0; a < space; a++) {
+                System.out.print("  ");
+            }
+
+            space++;
+
+            for (int b = 0; b < count; b++) {
+                System.out.print((char) letter + " ");
+            }
+
+            letter -= 2;
+            count -= 2;
+            System.out.println();
+        }
     }
 }
