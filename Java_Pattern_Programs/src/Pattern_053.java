f35767c2b931143fd6ff411ff017acc0c229f268
Yan <yan.bondarenko@gmail.com>
2019-12-14 21:41:02 +0200

    Starting 53rd pattern

---
+++ Java_Pattern_Programs/src/Pattern_053.java
@@ -0,0 +1,12 @@
+public class Pattern_053 {
+    public static void main(String[] args) {
+        /*
+        * 53
+        * A B C D E F G H I
+        *   A B C D E F G
+        *     A B C D E
+        *       A B C
+        *         A
+        * */
+    }
+}
